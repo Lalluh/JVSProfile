6e2e670a859ab4ece74c3023fae82524d34eaabc
Lunelitha <sousa159357@gmail.com>
2026-08-24 22:46:50 -0300

    Cards Principais e Formulário

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const modalContato = document.getElementById("modal-contato");
 const btnsConversar = document.querySelectorAll(".btn-primary");
 const btnFecharContato = document.getElementById("btn-close");
+const formContato = document.getElementById("form-contato");
 
 if (modalContato) {
   btnsConversar.forEach((botao) => {
@@ -16,6 +17,30 @@ if (modalContato) {
       modalContato.classList.remove("modal-active");
     });
   }
+}
+
+// Lógica do Formspree
+if (formContato) {
+  formContato.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const btnEnviar = formContato.querySelector(".btn-enviar");
+    btnEnviar.innerText = "ENVIANDO...";
+
+    const response = await fetch(formContato.action, {
+      method: "POST",
+      body: new FormData(formContato),
+      headers: { Accept: "application/json" },
+    });
+
+    if (response.ok) {
+      alert("Mensagem enviada com sucesso!");
+      formContato.reset();
+      modalContato.classList.remove("modal-active");
+    } else {
+      alert("Erro ao enviar a mensagem. Tente novamente!");
+    }
+    btnEnviar.innerText = "ENVIAR";
+  });
 }
 
 // Modal da Galeria
